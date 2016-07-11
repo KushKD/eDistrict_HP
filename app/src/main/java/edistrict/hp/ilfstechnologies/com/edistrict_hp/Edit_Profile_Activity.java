21b6a1e3fc17372fae3ff6ba82facc52e7d7e205
kuush <kushkumardhawan@gmail.com>
2016-07-11 11:34:04 +0530

    --Edit Profile Functionality Done --Services need to be implemented.

--- app/src/main/java/edistrict/hp/ilfstechnologies/com/edistrict_hp/Edit_Profile_Activity.java
+++ app/src/main/java/edistrict/hp/ilfstechnologies/com/edistrict_hp/Edit_Profile_Activity.java
@@ -5,62 +5,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import Model.User;
 
-public class User_Profile_Activity extends AppCompatActivity {
-
-    TextView user_profile_name_tv;
-    TextView user_profile_short_bio_tv;
-    TextView firstname_tv,middlename_tv,lastname_tv;
-    TextView firstname_father_tv, middlename_father_tv, lastname_father_tv;
-    TextView gender_tv, dateofbirth_tv , aadhaar_tv, family_id_tv,mobile_number_tv;
-
-    TextView state_tv,district_tv,tehsil_tv,village_tv,city_tv,block_tv,panchayat_tv,town_tv,ward_tv,address_tv;
-    TextView questionone_tv,questiontwo_tv,answerone_tv,answertwo_tv;
-    Button Back_bt;
-    ImageView edit_Profile_IV;
+public class Edit_Profile_Activity extends AppCompatActivity {
 
     User User_Data = null;
+    TextView user_profile_name_tv;
+    TextView user_profile_short_bio_tv;
+    EditText firstname_tv,middlename_tv,lastname_tv;
+    EditText firstname_father_tv, middlename_father_tv, lastname_father_tv;
+    EditText gender_tv, dateofbirth_tv , aadhaar_tv, family_id_tv,mobile_number_tv;
 
+    EditText state_tv,district_tv,tehsil_tv,village_tv,city_tv,block_tv,panchayat_tv,town_tv,ward_tv,address_tv;
+    EditText questionone_tv,questiontwo_tv,answerone_tv,answertwo_tv;
+    Button Back_bt, update_bt;
+    ImageView edit_Profile_IV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile_);
+        setContentView(R.layout.activity_edit_profile_);
         try {
             Intent getRoomDetailsIntent = getIntent();
-             User_Data = (User) getRoomDetailsIntent.getSerializableExtra("USER");
+            User_Data = (User) getRoomDetailsIntent.getSerializableExtra("USER");
         }catch(Exception e){
             Toast.makeText(getApplicationContext(),e.getLocalizedMessage().toString() ,Toast.LENGTH_SHORT).show();
         }
 
-try{
-    InitializeView();
-    setData();
+        try{
+            InitializeView();
+            setData();
 
 
-}catch(Exception e){
-    Toast.makeText(getApplicationContext(),"",Toast.LENGTH_LONG).show();
-}
+        }catch(Exception e){
+            Toast.makeText(getApplicationContext(),"",Toast.LENGTH_LONG).show();
+        }
         Back_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                User_Profile_Activity.this.finish();
-            }
-        });
-
-        edit_Profile_IV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //// TODO: 7/11/2016
-                Intent intent_Profile = new Intent();
-                intent_Profile.putExtra("USER", User_Data);
-                intent_Profile.setClass(User_Profile_Activity.this, Edit_Profile_Activity.class);
-                startActivity(intent_Profile);
+                Edit_Profile_Activity.this.finish();
             }
         });
     }
@@ -83,7 +71,7 @@ try{
         district_tv.setText(String.valueOf(User_Data.getDistrict().getName()));
         tehsil_tv.setText(String.valueOf(User_Data.getTehsil().getName()));
         village_tv.setText(String.valueOf(User_Data.getVillage()));
-       // city_tv.setText(String.valueOf(User_Data.get()));
+        // city_tv.setText(String.valueOf(User_Data.get()));
         block_tv.setText(String.valueOf(User_Data.getBlock()));
         panchayat_tv.setText(String.valueOf(User_Data.getPanchayat()));
         town_tv.setText(String.valueOf(User_Data.getTown().getName()));
@@ -99,34 +87,32 @@ try{
 
         user_profile_name_tv= (TextView)findViewById(R.id.user_profile_name);
         user_profile_short_bio_tv = (TextView)findViewById(R.id.user_profile_short_bio);
-        firstname_tv= (TextView)findViewById(R.id.firstname);
-        middlename_tv = (TextView)findViewById(R.id.middlename);
-        lastname_tv= (TextView)findViewById(R.id.lastname);
-        firstname_father_tv= (TextView)findViewById(R.id.firstname_father);
-        middlename_father_tv= (TextView)findViewById(R.id.middlename_father);
-        lastname_father_tv= (TextView)findViewById(R.id.lastname_father);
-        gender_tv= (TextView)findViewById(R.id.gender);
-        dateofbirth_tv= (TextView)findViewById(R.id.dateofbirth);
-        aadhaar_tv= (TextView)findViewById(R.id.aadhaarno);
-        family_id_tv = (TextView)findViewById(R.id.familyid);
-        mobile_number_tv = (TextView)findViewById(R.id.mobilenumber);
-        state_tv = (TextView)findViewById(R.id.state);
-        district_tv = (TextView)findViewById(R.id.district);
-        tehsil_tv = (TextView)findViewById(R.id.tehsil);
-        village_tv = (TextView)findViewById(R.id.village);
-        city_tv = (TextView)findViewById(R.id.city);
-        block_tv = (TextView)findViewById(R.id.block);
-        panchayat_tv = (TextView)findViewById(R.id.panchayat);
-        town_tv = (TextView)findViewById(R.id.town);
-        ward_tv = (TextView)findViewById(R.id.ward);
-        address_tv = (TextView)findViewById(R.id.address);
-        questionone_tv = (TextView)findViewById(R.id.questionone);
-        questiontwo_tv = (TextView)findViewById(R.id.questiontwo);
-        answerone_tv = (TextView)findViewById(R.id.answerone);
-        answertwo_tv = (TextView)findViewById(R.id.answertwo);
+        firstname_tv= (EditText)findViewById(R.id.firstname);
+        middlename_tv = (EditText)findViewById(R.id.middlename);
+        lastname_tv= (EditText)findViewById(R.id.lastname);
+        firstname_father_tv= (EditText)findViewById(R.id.firstname_father);
+        middlename_father_tv= (EditText)findViewById(R.id.middlename_father);
+        lastname_father_tv= (EditText)findViewById(R.id.lastname_father);
+        gender_tv= (EditText)findViewById(R.id.gender);
+        dateofbirth_tv= (EditText)findViewById(R.id.dateofbirth);
+        aadhaar_tv= (EditText)findViewById(R.id.aadhaarno);
+        family_id_tv = (EditText)findViewById(R.id.familyid);
+        mobile_number_tv = (EditText)findViewById(R.id.mobilenumber);
+        state_tv = (EditText)findViewById(R.id.state);
+        district_tv = (EditText)findViewById(R.id.district);
+        tehsil_tv = (EditText)findViewById(R.id.tehsil);
+        village_tv = (EditText)findViewById(R.id.village);
+       // city_tv = (EditText)findViewById(R.id.city);
+        block_tv = (EditText)findViewById(R.id.block);
+        panchayat_tv = (EditText)findViewById(R.id.panchayat);
+        town_tv = (EditText)findViewById(R.id.town);
+        ward_tv = (EditText)findViewById(R.id.ward);
+        address_tv = (EditText)findViewById(R.id.address);
+        questionone_tv = (EditText)findViewById(R.id.questionone);
+        questiontwo_tv = (EditText)findViewById(R.id.questiontwo);
+        answerone_tv = (EditText)findViewById(R.id.answerone);
+        answertwo_tv = (EditText)findViewById(R.id.answertwo);
         Back_bt = (Button)findViewById(R.id.back);
         edit_Profile_IV = (ImageView)findViewById(R.id.edit_Profile);
     }
-
-
 }
